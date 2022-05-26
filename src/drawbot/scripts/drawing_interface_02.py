3c89c47b930018f4b9d17b9922dfb9f3feb64cd6
Riccardo <giacchinoriccardo@outlook.it>
2022-05-26 11:26:45 +0200

    Modify the code for the interface we are gonna use for the robot

--- src/drawbot/scripts/drawing_interface_02.py
+++ src/drawbot/scripts/drawing_interface_02.py
@@ -2,20 +2,31 @@
 # Alternative: https://www.techwithtim.net/tutorials/python-module-walk-throughs/turtle-module/drawing-with-mouse/
 
 from turtle import *
-
 import turtle
-  
 
-ws = turtle.Screen()
-  
+class Coordinates:
+  x: int
+  y: int
+
+WIDTH, HEIGHT = 800, 400
+
+screen = turtle.Screen()
+screen.setup(WIDTH + 4, HEIGHT + 8)  # fudge factors due to window borders & title bar
+screen.setworldcoordinates(0, 0, WIDTH, HEIGHT)
+
+coord = Coordinates()
 
 def func(i, j):
+  
   turtle.goto(i, j)
-  turtle.write(str(i)+","+str(j))
+  coord.x = int(i)
+  coord.y = int(j)
+  turtle.write("x="+str(coord.x)+"\ny="+str(coord.y))
+  print(coord.x, coord.y)
   
 
-ws.onclick(func)
-ws.mainloop()
+screen.onclick(func)
+screen.mainloop()
 
 
 
